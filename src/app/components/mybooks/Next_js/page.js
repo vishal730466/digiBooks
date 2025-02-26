84f53be3e3c0b9b9f3e83b3c87d9842a1cd1bdcc
vishale <vishalingle9876@gmail.comm>
2025-02-26 11:49:46 +0530

    mobile version

--- src/app/components/mybooks/Next_js/page.js
+++ src/app/components/mybooks/Next_js/page.js
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useEffect, useRef, useState } from 'react'
-import "./style.css"
 
 import { MdNavigateNext } from "react-icons/md";
 import { MdArrowBackIos } from "react-icons/md";
